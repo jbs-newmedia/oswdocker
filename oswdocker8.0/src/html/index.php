4f6fc3465ab50451f089c503ca3a087f110823e5
JBS New Media GmbH <info@jbs-newmedia.de>
2020-11-04 19:08:53 +0100

    with php 8.0RC3

---
+++ oswdocker8.0/src/html/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is a demo
+ *
+ * @author Juergen Schwind
+ * @copyright Copyright (c) JBS New Media GmbH - Juergen Schwind (https://jbs-newmedia.com)
+ * @package osWFrame
+ * @link https://oswframe.com
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+phpinfo();
+
+?>
